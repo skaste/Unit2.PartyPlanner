f12cff76ced0caf2ee42e14efdf8ea74335826ce
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-07 11:18:07 -0600

    Created the input and add new event buttons and placed an h2 in the form

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // console.log(`Party test`)
 //user is able to find the name of events happening
+//a delete button is added next to each event that removes it from the list
+//user can submit a new event to the site
+//user observes their party added to the list of event
+
+const getAllEvents = async () => {
+  console.log(getAllEvents);
+  
+};
